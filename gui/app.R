91d666a355fd7611e89208d22ca00af8f83a37ed
brettljausn <flx.lechleitner@gmail.com>
2018-07-21 13:51:40 +0200

    added rsconnect to deploy app added contact-info in gui

--- gui/app.R
+++ gui/app.R
@@ -1,13 +1,37 @@
 ## app.R ##
 library(shiny)
 library(shinydashboard)
+library(rsconnect)
 
 ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody()
+  dashboardHeader(title = "REACTIONATOR"),
+  dashboardSidebar(sidebarMenu(
+    menuItem(
+      "Kontakt/Feedback",
+      tabName = "contact",
+      icon = icon("envelope")
+    )
+  )),
+  dashboardBody(tabItems(tabItem(
+    tabName = "contact",
+    fluidRow(box(title = "E-Mail", uiOutput("email")),
+             box(title = "GitHub", uiOutput("github")))
+  )))
 )
 
-server <- function(input, output) { }
+server <- function(input, output) {
+  # create mail-link and output for ui
+  email <-
+    a("flx.lechleitner@gmail.com", href = "mailto:flx.lechleitner@gmail.com")
+  output$email <- renderUI({
+    tagList(email)
+  })
+  
+  # create github-link and output for ui
+  github <- a("https://github.com/brettljausn/reactionator/", href = "https://github.com/brettljausn/reactionator/")
+  output$github <- renderUI({tagList("GitHub-Repository: ", github)})
+
+  
+}
 
 shinyApp(ui, server)
